42eaaf070f78c0942aed16e165a19c63415b4f2b
AnyaV38 <43682485+AnyaV38@users.noreply.github.com>
2019-07-10 21:03:42 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {style} from './styleApp.css';
+import Header from './components/Header.js';
+import Text from './components/Text.js';
+
+
+const App = () => {
+    return(
+        <div className='app-wrapper' > 
+
+            <Header />
+            <Text />
+            
+
+        </div>
+    
+    );
+    
+}
+export default App;
