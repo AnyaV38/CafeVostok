58ba12d934679f04e48362cb2b1db7aa0d1c15b1
AnyaV38 <43682485+AnyaV38@users.noreply.github.com>
2019-07-11 15:49:24 +0400

    Add files via upload

---
+++ src/components/Phone.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Phone = ()=> {
+    return(
+        <phone className='phone'>
+
+            <img src='https://static.tildacdn.com/tild6630-6561-4231-b662-356662666238/icsArtboard1copy.svg' alt="" />
+            <h1>+1 234 567 89 00</h1>
+
+             </phone>
+        
+    )
+}
+ export default Phone ;
---
+++ src/components/Rezhim.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Rezhim = ()=> {
+    return(
+        <rezhim className='rezhim'>
+
+            <img src='https://static.tildacdn.com/tild3533-3935-4264-a336-366462323431/icsArtboard1.svg' alt="" />
+            <h2>Мы открыты: 7:30 - 16:00</h2>
+
+             </rezhim>
+        
+    )
+}
+ export default Rezhim ;
