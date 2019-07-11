b5dcef43ee34189b602ae2ee4e2b227b45eeda1f
AnyaV38 <43682485+AnyaV38@users.noreply.github.com>
2019-07-11 13:34:53 +0400

    Add files via upload

---
+++ src/components/Text.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Text = ()=> {
+    return(
+        <text className='text'>
+            <span>КАФЕ АВТОРСКОЙ КУХНИ</span>
+            <span1>Лёгкая атмосфера, приятные цены, вкусная еда</span1>
+            <span2>Контакты</span2>
+            <span3>Новости</span3>
+            <span4>Меню</span4>
+            <span5>О кафе</span5>
+            </text>
+    )
+}
+ export default Text ;
