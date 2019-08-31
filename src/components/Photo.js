4c0b2c789798a9b2a1a9f4fa129e0ddd4ac5ab9d
AnyaV38 <43682485+AnyaV38@users.noreply.github.com>
2019-08-31 16:19:20 +0400

    Add files via upload

---
+++ src/components/Photo.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Photo = ()=> {
+    return(
+        <photo className='photo'>
+
+            <a>
+            <img src ="https://sun9-13.userapi.com/c852032/v852032658/195c4a/JyYCpiE7NCM.jpg"  width="50%" height="100%"/>
+            <img src ="https://sun9-50.userapi.com/c854428/v854428658/d6a11/qcjOvcuG7_A.jpg" width="50%" height="100%"/>
+            </a>
+            <b>
+                <img src="https://sun9-41.userapi.com/c856128/v856128943/28cce/oMVY1XQu5c8.jpg" width="50%" height="100%"/>
+                <img src="https://sun9-18.userapi.com/c849228/v849228325/16f12c/o7B5tZQtpj0.jpg" width="50%" height="100%"/>
+                <img src="https://sun9-8.userapi.com/c855528/v855528514/308d1/x6SIWyi9T0U.jpg" width="50%" height="100%"/>
+                <img src="https://sun9-41.userapi.com/c851224/v851224658/1b9820/Brn0GGwRdgs.jpg" width="50%" height="100%"/>
+                <img src="https://sun9-24.userapi.com/c854128/v854128658/d7d1d/qLVAKTU5ZYM.jpg" width="50%" height="100%"/>
+                <img src="https://sun9-15.userapi.com/c850228/v850228658/1c9c74/EngF6AE34YQ.jpg" width="50%" height="100%"/>
+                <img src="https://sun9-22.userapi.com/c844418/v844418658/20652f/AW_3bwA20Z0.jpg" width="50%" height="415"/>
+                <img src="https://sun9-50.userapi.com/c854216/v854216141/3391/Y30zySb68nM.jpg" width="50%" height="100%"/>
+            </b>
+            <c>
+                <img src="http://ds4.edushd.ru/media/upload/gramoti/cook1.jpeg" width="100%" height="100%"/>
+            </c>
+            <d>
+                <img src="https://sun9-38.userapi.com/c851416/v851416072/12c890/1BaNhr512l0.jpg" width="50%" height="100%"/>
+                <img src="https://sun9-41.userapi.com/c854228/v854228072/519c1/EulX1cS7IAY.jpg" width="50%" height="100%"/>
+                <img src="https://sun9-9.userapi.com/c857428/v857428181/1ca08/9sALsVB5fYA.jpg" width="50%" height="100%"/>
+                <img src="https://sun9-10.userapi.com/c855024/v855024915/d7847/wz4mIBV6Nk8.jpg" width="50%" height="100%"/>
+                <img src="https://sun9-36.userapi.com/c851028/v851028291/164639/aZ1dXroBn_8.jpg" width="50%" height="100%"/>
+                <img src="https://sun9-48.userapi.com/c851224/v851224658/1b97d0/4Wdi0UzJ0V0.jpg" width="50%" height="100%"/>
+                
+            </d>
+
+             </photo>
+        
+    )
+}
+ export default Photo ;
